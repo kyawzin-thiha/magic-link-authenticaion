e09776cf9db218900ee78107c9c6e7b481f565f3
Kyaw Zin Thiha <hello@kyawzinthiha.com>
2023-03-21 01:18:38 +0000

    disable cors for good 😀

--- api/src/routes/magicLink/magicLink.module.ts
+++ api/src/routes/magicLink/magicLink.module.ts
@@ -1,24 +1,11 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MagicLinkController } from './magicLink.controller';
 import { MagicLinkService } from './magicLink.service';
 import { HelperModule } from 'src/helper/helper.module';
 import { DbModule } from 'src/db/db.module';
-import * as cors from 'cors';
 @Module({
   imports: [HelperModule, DbModule],
   controllers: [MagicLinkController],
   providers: [MagicLinkService],
 })
-export class MagicLinkModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(
-        cors({
-          origin: '*',
-          credentials: false,
-        }),
-      )
-      .exclude('magic-link/auth/*')
-      .forRoutes('magic-link/*');
-  }
-}
+export class MagicLinkModule { }
